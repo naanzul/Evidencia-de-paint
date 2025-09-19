75fde615a37a44bda11b65e703572a522b24fd84
a1663116-blip <a01663116@tec.mx>
2025-09-19 13:31:53 -0600

    se modifico

---
+++ paint2.py
@@ -0,0 +1,129 @@
+from turtle import *
+from freegames import vector
+
+
+def line(start, end):
+    """Draw line from start to end."""
+    up()
+    goto(start.x, start.y)
+    down()
+    goto(end.x, end.y)
+
+
+def square(start, end):
+    """Draw square from start to end."""
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(4):
+        forward(end.x - start.x)
+        left(90)
+
+    end_fill()
+
+
+def circle(start, end):
+    """Draw circle from start to end."""
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    
+    radius = ((end.x - start.x) ** 2 + (end.y - start.y) ** 2) ** 0.5
+    turtle_circle(radius)
+
+    end_fill()
+
+
+def rectangle(start, end):
+    """Draw rectangle from start to end."""
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    width = end.x - start.x
+    height = end.y - start.y
+
+    for _ in range(2):
+        forward(width)
+        left(90)
+        forward(height)
+        left(90)
+
+    end_fill()
+
+
+def triangle(start, end):
+    """Draw triangle from start to end."""
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    goto(end.x, end.y)
+    goto(start.x - (end.x - start.x), end.y)
+    goto(start.x, start.y)
+
+    end_fill()
+
+
+def tap(x, y):
+    """Store starting point or draw shape."""
+    start = state['start']
+
+    if start is None:
+        state['start'] = vector(x, y)
+    else:
+        shape = state['shape']
+        end = vector(x, y)
+        shape(start, end)
+        state['start'] = None
+
+
+def store(key, value):
+    """Store value in state at key."""
+    state[key] = value
+
+
+def turtle_circle(radius):
+    """Draw a circle using turtle at current position."""
+    # Save current position and heading
+    pos = pos()
+    heading = turtle.heading()
+
+    turtle.setheading(0)
+    turtle.circle(radius)
+
+    # Restore position and heading
+    turtle.setheading(heading)
+    turtle.setpos(pos)
+
+
+state = {'start': None, 'shape': line}
+setup(420, 420, 370, 0)
+onscreenclick(tap)
+listen()
+
+# Colores
+onkey(lambda: color('black'), 'K')
+onkey(lambda: color('white'), 'W')
+onkey(lambda: color('green'), 'G')
+onkey(lambda: color('blue'), 'B')
+onkey(lambda: color('red'), 'R')
+onkey(lambda: color('purple'), 'P')  # NUEVO COLOR
+
+# Formas
+onkey(lambda: store('shape', line), 'l')
+onkey(lambda: store('shape', square), 's')
+onkey(lambda: store('shape', circle), 'c')
+onkey(lambda: store('shape', rectangle), 'r')
+onkey(lambda: store('shape', triangle), 't')
+
+# Función para deshacer
+onkey(undo, 'u')
+
+done()
+
